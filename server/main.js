47bf8c393d1151e14fc4bf954030fe09143564c3
Mr. Metelus <Mdmetelus@gmail.com>
2018-12-16 17:37:46 -0500

    "update"

--- server/main.js
+++ server/main.js
@@ -2,8 +2,22 @@ var express = require('express');
 
 var app = new express();
 
+// added boddy parser middleware for mor complcated requests like post and etc.
+var parser = require('bady-parser');
+
 app.get('/', (req, res) => {
     console.log('Working!');
     res.render('./../app/index.ejs',{});
 })
+.use(express.static(--dirname + '/../app'))
 .listen(7777);
+
+
+// middleware
+// handles json requestds
+app.use(parser.json());
+//handles post reequests with express
+app.use(parser.urlencoded({extended:false}));
+// this middleware allows us to interact with the back end from the front end of our app
+
+require('./routes/items.js')(app);
